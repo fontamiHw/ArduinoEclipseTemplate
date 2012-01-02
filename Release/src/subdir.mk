b9b969d5030c5429cad642041b9cb4524e0a46a1
Michele Fontanella <fontami68_reg@yahoo.it>
2012-01-02 20:30:20 +0100

    first commit

---
+++ Release/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/main.cpp \
+../src/memory.cpp 
+
+OBJS += \
+./src/main.o \
+./src/memory.o 
+
+CPP_DEPS += \
+./src/main.d \
+./src/memory.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"E:\Win\hw\arduino-0022\hardware\arduino\cores\arduino" -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//www.b2cqshop.com
+
+#include <WProgram.h>
+
+
+
+void setup(){
+
+}
+
+void loop(){}
+
+int main(void) {
+
+    /* Must call init for arduino to work properly */
+    init();
+    setup();
+
+    for (;;) {
+        loop();
+    } // end for
+} // end main
